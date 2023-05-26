50a5d15e7aea18257389b88d70d88a01641cec06
Lê Trí Tâm <tam.le@techsoft.asia>
2023-05-26 16:57:36 +0700

    draggable on strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
-import 'unfonts.css';
-import { ConfigProvider } from 'antd';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import "unfonts.css";
+import { ConfigProvider } from "antd";
 
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import { store, persist } from './store/index.ts';
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persist } from "./store/index.ts";
 
 const ana_theme = {
   token: {
-    primaryColor: '#4D4479',
+    primaryColor: "#4D4479",
     fontFamily: "'WorkSans', sans-serif",
   },
 };
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <Provider store={store}>
     <PersistGate persistor={persist}>
       <React.StrictMode>
@@ -25,5 +25,5 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
         </ConfigProvider>
       </React.StrictMode>
     </PersistGate>
-  </Provider>,
+  </Provider>
 );
--- src/pages/playground/components/Draggable.tsx
+++ src/pages/playground/components/Draggable.tsx
@@ -1,6 +1,13 @@
 import { initialState } from "./initialState";
 import { useState } from "react";
-import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import {
+  DragDropContext,
+  Droppable,
+  Draggable,
+  OnDragEndResponder,
+  DropResult,
+} from "react-beautiful-dnd";
+import { StrictModeDroppable } from "./StrictModeDroppable";
 
 interface ChapterProps {
   name?: string;
@@ -20,9 +27,7 @@ const Chapter = ({ id, content, index }: ChapterProps) => {
     <Draggable draggableId={id} index={index}>
       {(provided) => (
         <div
-          id={id}
-          className="border-black  border-2 p-4 w-full h-fit text-lg"
-          draggable
+          className="border-black  border-2 p-4 w-full h-fit text-lg transition-[300ms]"
           {...provided.dragHandleProps}
           {...provided.draggableProps}
           ref={provided.innerRef}
@@ -36,42 +41,125 @@ const Chapter = ({ id, content, index }: ChapterProps) => {
 
 const Journey = ({ id, title, chapters }: JourneyProps) => {
   return (
-    <div className="border-black border-2 flex flex-col p-4 gap-6 w-fit h-fit">
+    <div className="border-black border-2 transition-[300ms] flex flex-col p-4 gap-6 w-fit h-fit min-w-[500px] min-h-[200px]">
       <h2 className="text-2xl">{title}</h2>
-      <Droppable droppableId={id}>
+      <StrictModeDroppable droppableId={id}>
         {(provided) => {
           return (
             <div
-              id={id}
-              className="flex flex-col gap-2 w-full h-full"
+              className="flex flex-col gap-2 w-full h-full grow transition-[300ms]"
               ref={provided.innerRef}
               {...provided.droppableProps}
             >
               {chapters.map((chapter, index) => {
                 return (
-                  <div key={chapter.id}>
-                    <Chapter
-                      id={chapter.id}
-                      content={chapter.content}
-                      index={index}
-                    />
-                  </div>
+                  <Chapter
+                    key={chapter.id}
+                    id={chapter.id}
+                    content={chapter.content}
+                    index={index}
+                  />
                 );
               })}
               {provided.placeholder}
             </div>
           );
         }}
-      </Droppable>
+      </StrictModeDroppable>
     </div>
   );
 };
 
 const Dragging = () => {
   const [dragState, setDragState] = useState(initialState);
-  const onDragEnd = () => {
-    //to do
+
+  // interface ResultProps {
+  //   draggableId: string;
+  //   type: string;
+  //   reason: string;
+  //   source: {
+  //     droppableId: string;
+  //     index: number;
+  //   };
+  //   destination: {
+  //     droppableId: "column-1";
+  //     index: 1;
+  //   };
+  // }
+
+  const onDragEnd = (result: DropResult): void => {
+    const { destination, source, draggableId } = result;
+
+    if (!destination) {
+      return;
+    }
+
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+
+    const start = dragState.journeys.find(
+      (journey) => journey.id === source.droppableId
+    ) ?? { id: "", title: "", chapters: [""] };
+    const finish = dragState.journeys.find(
+      (journey) => journey.id === destination.droppableId
+    ) ?? { id: "", title: "", chapters: [""] };
+
+    if (start === finish) {
+      const newChapters = Array.from(start.chapters);
+      newChapters.splice(source.index, 1);
+      newChapters.splice(destination.index, 0, draggableId);
+
+      const newJourney = {
+        ...start,
+        chapters: newChapters,
+      };
+
+      const newJourneys = dragState.journeys.map((journey) => {
+        if (journey.id !== newJourney.id) return journey;
+        return newJourney;
+      });
+
+      const newState = {
+        ...dragState,
+        journeys: newJourneys,
+      };
+
+      setDragState(newState);
+      return;
+    }
+
+    // Moving from one list to another
+    const startChapters = Array.from(start.chapters);
+    startChapters.splice(source.index, 1);
+    const newStartJourney = {
+      ...start,
+      chapters: startChapters,
+    };
+
+    const finishChapters = Array.from(finish.chapters);
+    finishChapters.splice(destination.index, 0, draggableId);
+    const newFinishJourney = {
+      ...finish,
+      chapters: finishChapters,
+    };
+
+    const newJourneys = dragState.journeys.map((journey) => {
+      if (journey.id === newStartJourney.id) return newStartJourney;
+      if (journey.id === newFinishJourney.id) return newFinishJourney;
+      return journey;
+    });
+
+    const newState = {
+      ...dragState,
+      journeys: newJourneys,
+    };
+    setDragState(newState);
   };
+
   return (
     <DragDropContext onDragEnd={onDragEnd}>
       <div className="flex flex-row gap-6">
